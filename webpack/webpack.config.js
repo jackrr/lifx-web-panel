741f34f151cd41d18d4e80b9a1a0dd0603210b18
Jack Ratner <jackratner@gmail.com>
2016-06-11 13:52:49 -0400

    initial webpack/babel/react stack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+   entry: './src/app.js',
+   output: {
+    path: './bin',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
+};
